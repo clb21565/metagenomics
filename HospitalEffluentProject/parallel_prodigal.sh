c883a1812c3fb0fb61f30b9ca0efe2744a3d8b4f
Connor Brown <35241700+clb21565@users.noreply.github.com>
2022-03-26 12:40:37 -0400

    Update parallel_prodigal.sh

--- HospitalEffluentProject/parallel_prodigal.sh
+++ HospitalEffluentProject/parallel_prodigal.sh
@@ -4,16 +4,16 @@
 #SBATCH -A prudenlab
 #SBATCH -N 1 
 #SBATCH -n 32
-#source activate biopy 
-source activate prodigal
+
 module load parallel/20180222
+
 function hmmer() {
     n=$(basename "$1")
-    #hmmsearch -Z 2000000 --domZ 89 --cpu 1 -o $1.output.hmmsearch.txt cpr_43_markers.hmm $1
-prodigal -a ${1}.faa -d ${1}.fna -p meta -i $1
+    prodigal -a ${1}.faa -d ${1}.fna -p meta -i $1
 }
 
-export -f hmmer
-find . -maxdepth 1 -type f -name "x*" | parallel -j 32 hmmer {}
+samples=sample
+split -n l/32 $sample.fasta
 
-
+export -f pprodigal
+find . -maxdepth 1 -type f -name "x*" | parallel -j 32 pprodigal {}
